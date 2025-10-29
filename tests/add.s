c6f7a32323b5c8b3b871aade11f69b014afe8302
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-10-29 21:19:04 +0000

    update asm example

--- tests/add.s
+++ tests/add.s
@@ -1,24 +1,28 @@
     	.data
 msg1: 	.asciiz "Introduza 2 numeros inteiros: "
-msg2: 	.asciiz "A soma dos dois numeros é: "
+msg2:   .asciiz "Introduza o segundo numero: "
+msg3: 	.asciiz "A soma dos dois numeros é: "
 
     	.text
 
 li $v0, 4 # v0=4 é print_str
 la $a0, msg1
 syscall
+
 li $v0, 5 # v0=5 é read_int
 syscall
-
 add $t0, $zero, $v0 # t0 = a, add com zero é so um mov
+
+la $a0, msg2
+li $v0, 4
+syscall
 
 li $v0, 5
 syscall
-
 add $t1, $zero, $v0 # t1 = b
 
 
-la $a0, msg2
+la $a0, msg3
 
 li $v0,4
 syscall
