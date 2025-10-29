708b3b52801d32c9b30835acf40590471cc20d74
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-10-29 21:14:36 +0000

    remove tests

--- src/main.zig
+++ src/main.zig
@@ -28,11 +28,3 @@ pub fn main() !void {
         exec.execute(instr, &cpu, &mem, &parsed.labels);
     }
 }
-
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
