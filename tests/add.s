0cdfbd5935e31a763187e932766c986462bf2852
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-10-30 23:25:25 +0000

    translate the asm program to english

--- tests/add.s
+++ tests/add.s
@@ -1,17 +1,17 @@
     	.data
-msg1: 	.asciiz "Introduza 2 numeros inteiros: "
-msg2:   .asciiz "Introduza o segundo numero: "
-msg3: 	.asciiz "A soma dos dois numeros é: "
+msg1: 	.asciiz "Enter the first number: "
+msg2:   .asciiz "Enter the second number: "
+msg3: 	.asciiz "The sum is: "
 
     	.text
 
-li $v0, 4 # v0=4 é print_str
+li $v0, 4 # v0=4 is print_str
 la $a0, msg1
 syscall
 
-li $v0, 5 # v0=5 é read_int
+li $v0, 5 # v0=5 is read_int
 syscall
-add $t0, $zero, $v0 # t0 = a, add com zero é so um mov
+add $t0, $zero, $v0 # t0 = a, add with zero is just move
 
 la $a0, msg2
 li $v0, 4
@@ -27,6 +27,6 @@ la $a0, msg3
 li $v0,4
 syscall
 
-add $a0, $t0, $t1 # a0 é parametro de entrada para o v0 que é o print int
+add $a0, $t0, $t1 # a0 is the input parameter for print_int
 li $v0,1
 syscall
