785f5d4af2eba890ed07a724f465c2af54f5bb33
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-10-30 23:18:55 +0000

    add astro-compress

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import playformCompress from "@playform/compress";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://lucascompython.github.io/",
   base: "/mipster",
+  integrations: [playformCompress()],
 });
